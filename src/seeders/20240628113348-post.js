10769250000583b5164c324e1841c8ace44603c6
saadiqbal19114 <saadiqbal19114@gmail.com>
2024-07-01 16:23:45 +0500

    post test cases completed

--- src/seeders/20240628113348-post.js
+++ src/seeders/20240628113348-post.js
@@ -12,18 +12,22 @@ module.exports = {
       'posts',
       [
         {
+          uuid:"64abb336-4a72-4b9d-ad79-0a873bf15933",
           body: 'This is the first post. It contains some interesting content.',
           userId: userRows[0].id,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
+          uuid:"7ff7c406-afa3-4c54-9a84-4fa6a1850a04",
           body: "Here's another post. It's unique and can't be empty.",
           userId: userRows[1].id,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
+          uuid:"9972246c-2bdc-489e-baf4-fc354912bb23",
+          
           body: 'The third post is just as important as the first two!',
           userId: userRows[0].id,
           createdAt: new Date(),
--- src/test/controllers/post.test.js
+++ src/test/controllers/post.test.js
@@ -7,7 +7,7 @@ afterAll(async () => {
 });
 
 describe('Get all posts', () => {
-  it('Should return array of users.', async () => {
+  it('Should return array of post.', async () => {
     let res = await request(app).get('/post');
     expect(res.status).toBe(200);
     expect(res.body.length).toBeGreaterThanOrEqual(0);
@@ -15,84 +15,50 @@ describe('Get all posts', () => {
   });
 });
 
-describe('Get User by id', () => {
-  test('Should return the object of users of the given id.', async () => {
-    let res = await request(app).get(
-      '/user/id/7bf6ee45-f804-472e-a9c7-5e6612f74fce',
-    );
+describe('Get post with user', () => {
+  test('Should return the object of ost of the given id.', async () => {
+    let res = await request(app).get('/post/post-with-user');
     expect(res.status).toBe(200);
     expect(typeof res.body).toBe('object');
-    expect(res.body.uuid).toBe('7bf6ee45-f804-472e-a9c7-5e6612f74fce');
-    expect(Object.keys(res.body).length).toBe(7);
-  });
-
-  it("Should return error if uuid doesn't exist.", async () => {
-    let res = await request(app).get(
-      '/user/id/aad16615-9f86-41ce-824f-9ed9cf5eec02',
-    );
-    expect(res.status).toBe(404);
-    expect(res.error).toEqual(expect.any(Error));
   });
 });
 
-describe('Create a user', () => {
-  it('should return a success message when a user is created', async () => {
-    let res = await request(app).post('/user').send({
-      name: 'saad',
-      email: 'saad@gmail.com',
-      role: 'student',
+describe('Create a post', () => {
+  it('should return a success message when a post is created', async () => {
+    let res = await request(app).post('/post').send({
+      body: 'this is body',
+      userUuid: '7bf6ee45-f804-472e-a9c7-5e6612f74fce',
     });
     expect(res.status).toBe(201);
   });
-
-  it('should return an error if required field is missing', async () => {
-    let res = await request(app).post('/user').send({
-      // name: '',
-      email: 'saad@gmail.com',
-      role: 'student',
-    });
-    expect(res.status).toBe(400);
-    expect(res.error).toEqual(expect.any(Error));
-  });
-  it("Should return error if any field's type is not correct.", async () => {
-    let res = await request(app).post('/user').send({
-      name: 123,
-      email: 'saad@gmail.com',
-      role: 'student',
-    });
-    expect(res.status).toBe(400);
-    expect(res.error).toEqual(expect.any(Error));
-  });
 });
 
-describe('Update user details for the given uuid', () => {
-  it('should return a success message when a record is updated', async () => {
+describe('Update post details for the given uuid', () => {
+  it('should return a success message when a post is updated', async () => {
     let res = await request(app)
-      .put('/user/id/7bf6ee45-f804-472e-a9c7-5e6612f74fce')
+      .put('/post/id/64abb336-4a72-4b9d-ad79-0a873bf15933')
       .send({
-        name: 'iqbal',
-        email: 'saad@gmail.com',
-        role: 'student',
+        body: 'this is body',
+        userId:1,
       });
-    expect(res.status).toBe(201);
+    expect(res.status).toBe(200);
   });
 
   it("Should return error if id doesn't exist in records.", async () => {
     let res = await request(app)
-      .put('/user/id/95f93b16-c481-4589-ba0b-3267dd1b2d50')
+      .put('/post/id/12a3dbb0-ad7a-472c-9db7-2fa89a8f2415')
       .send({
-        name: 'iqbal',
-        email: 'saad@gmail.com',
-        role: 'student',
+        body: 'this is body',
+        userId: 1,
       });
     expect(res.status).toBe(400);
   });
 });
 
-describe('Delete the user record for given id', () => {
+describe('Delete the post record for given id', () => {
   it("Should return error if id doesn't exist.", async () => {
     let res = await request(app).delete(
-      '/user/id/95f93b16-c481-4589-ba0b-3267dd1b2d50',
+      '/post/id/95f93b16-c481-4589-ba0b-3267dd1b2d50',
     );
     expect(res.status).toBe(404);
   });
